6c472941d87e8b5a002420fa93eb71c9cb79d800
Francisco <francisco_nascimento@outlook.com.br>
2018-01-21 17:07:19 -0300

    Implementação do Batalha Naval, falta implementar a parte de WS e CI

---
+++ BatalhaNaval-WSCI/src/batalhanaval/wsci/Partida.java
@@ -0,0 +1,45 @@
+package batalhanaval.wsci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francisco
+ */
+public class Partida {
+
+    private static int numNavios = 3;
+    private static int pontuacao = 0;
+    private boolean vez = false;
+    private boolean terminou = true;
+    private int entradaJogador2;
+
+    public Partida() {
+
+    }
+
+    public void iniciarPartida(){
+
+        Jogador jogador1 = new Jogador("Jogador1", pontuacao, numNavios);
+        Jogador jogador2 = new Jogador("Jogador2", pontuacao, numNavios);
+        Scanner entrada = new Scanner(System.in);
+        int linha, coluna;
+        do {
+            System.out.println("Vez do jodador 1: ");
+            System.out.println("Digite o numero da linha: ");
+            linha = entrada.nextInt();
+            System.out.println("Digite o numero da coluna: ");
+            coluna = entrada.nextInt();
+            jogador1.atirar(jogador2, linha, coluna);
+
+            System.out.println("Vez do jodador 2: ");
+            System.out.println("Digite o numero da linha: ");
+            linha = entrada.nextInt();
+            System.out.println("Digite o numero da coluna: ");
+            coluna = entrada.nextInt();
+            jogador2.atirar(jogador1, linha, coluna);
+
+        } while (terminou);
+    }
+
+}
